23761f64259dac9c06eeb446ab1b2bb86dbdb1e5
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-16 19:30:53 +0530

    refactor : updated hooks

--- src/hooks/useSessionContext.ts
+++ src/hooks/useSessionContext.ts
@@ -1,8 +1,12 @@
-import { useContext } from "react"
-import {SessionContext} from "../context/SessionContext"
+import { useContext } from "react";
+import { SessionContext } from "../context/SessionContext";
 
-const useSessionContext=()=>{
-    return useContext(SessionContext);
-}
+const useSessionContext = () => {
+    const sessionContext = useContext(SessionContext);
+    if (sessionContext === undefined) {
+        throw new Error("Context is undefined");
+    }
+    return sessionContext;
+};
 
 export default useSessionContext;

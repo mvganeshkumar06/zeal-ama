06ff9418838ada4e70503cc364c4e847ef76dcb4
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-16 19:31:49 +0530

    refactor : updated app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { ZealProvider } from "@zeal-ui/core";
 import Routes from "./Routes/Router";
 import { SessionProvider } from "./context/SessionProvider";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const App = () => {
+    const domain = process.env.REACT_APP_DOMAIN as string;
+    const clientId = process.env.REACT_APP_CLIENT_ID as string;
+
     return (
-        <ZealProvider>
-            <SessionProvider>
-                <Routes />
-            </SessionProvider>
-        </ZealProvider>
+        <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            redirectUri={window.location.origin}
+        >
+            <ZealProvider>
+                <SessionProvider>
+                    <Routes />
+                </SessionProvider>
+            </ZealProvider>
+        </Auth0Provider>
     );
 };
 

7fd2abf346d3d5657fc831e3693167d23cb12a2a
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-18 21:00:40 +0530

    refactor : updated reducer

--- src/reducer/reducer.ts
+++ src/reducer/reducer.ts
@@ -1,11 +1,16 @@
 import { StateType, ActionType } from "../types/index";
 
-const reducer = (state: StateType, action: ActionType) => {
+const reducer = (state: StateType, action: ActionType): StateType => {
     switch (action.type) {
         case "SET_USERNAME":
             return {
                 ...state,
                 userName: action.payload,
+            };
+        case "SET_USER_SOCKET_ID":
+            return {
+                ...state,
+                userSocketId: action.payload,
             };
         case "SET_SESSION_ID":
             return {
@@ -32,10 +37,20 @@ const reducer = (state: StateType, action: ActionType) => {
                 ...state,
                 hostStream: action.payload,
             };
+        case "SET_SESSION_CHAT":
+            return {
+                ...state,
+                session: { ...state.session, chats: action.payload },
+            };
         case "SET_IS_LOADING":
             return {
                 ...state,
                 isLoading: { ...state.isLoading, ...action.payload },
+            };
+        case "SET_IS_ERROR":
+            return {
+                ...state,
+                isError: { ...state.isError, ...action.payload },
             };
         default:
             return state;
